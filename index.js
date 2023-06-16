e0e9825afa5290c14c1880c5327cd237c7e6450b
tmax818 <tylermaxwell661@gmail.com>
2023-06-16 11:29:13 -0600

    first sol

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 
+for(let i = 1; i !== 16; i++){
+    if(!(i % 15)){
+        console.log("FizzBuzz")
+    } else if(!(i % 5)){
+        console.log("Buzz")
+    } else if(!(i % 3)) {
+        console.log("Fizz")
+    } else {
+        console.log(i)
+    }
+
+}
